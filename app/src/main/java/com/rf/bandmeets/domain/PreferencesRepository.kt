8d404b48d1a549fa52f7773bdec12ee757ebd704
rf19l <67389271+rf19l@users.noreply.github.com>
2022-02-08 15:09:26 -0500

    Added Hilt base app and activity as well as boilerplate to be updated after initial setup is completed.

---
+++ app/src/main/java/com/rf/bandmeets/domain/PreferencesRepository.kt
@@ -0,0 +1,18 @@
+package com.rf.bandmeets.domain
+
+import androidx.annotation.Nullable
+
+interface PreferencesRepository {
+    /*
+    fun getPreferences(@Nullable fetchFresh: Boolean?): Observable<Preferences?>?
+
+    fun savePreferences(preferences: Preferences?)
+
+    fun clearPreferences()
+
+    fun partialUpdate(preferences: Preferences?): Observable<Preferences?>?
+
+    fun setPin(pin: String?): Observable<UseCaseResponse<Boolean?>?>?
+
+     */
+}
