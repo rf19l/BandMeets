ac82954456a5f84dc7a11b0772afd94c0ad789fb
Ryan.Foster <ryan.foster@publix.com>
2022-04-21 12:37:04 -0400

    Code cleaning

--- app/src/main/java/com/rf/bandmeets/MainActivity.kt
+++ app/src/main/java/com/rf/bandmeets/MainActivity.kt
@@ -3,12 +3,8 @@ package com.rf.bandmeets
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rf.bandmeets.ui.theme.BandMeetsTheme
 import dagger.hilt.android.AndroidEntryPoint
