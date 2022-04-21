aa7c9c203680f154c4efc2967c11513175fc0da0
Ryan.Foster <ryan.foster@publix.com>
2022-04-21 14:16:46 -0400

    Added top app bar and back back arrow

--- app/src/main/java/com/rf/bandmeets/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/rf/bandmeets/ui/screens/HomeScreen.kt
@@ -1,14 +1,42 @@
 package com.rf.bandmeets.ui.screens
 
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.rf.bandmeets.navigation.NavigationUtils
 
 @Composable
-fun HomeScreen(navController: NavController){
-    Button(onClick = {navController.navigate(Screen.SplashScreen.route)}){
-        Text(text="To Splash Screen")
-    }
+fun HomeScreen(navController: NavController) {
+    Scaffold(topBar = {
+        TopAppBar(elevation = 5.dp) {
+            Row(horizontalArrangement = Arrangement.Start) {
+                Icon(imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Arrow Back",
+                    modifier = Modifier.clickable {
+                        NavigationUtils.popBack(navController)
+                    })
+                Spacer(modifier = Modifier.width(25.dp))
 
+                Text(
+                    text = "Login",
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+        }
+    }) {
+        Button(onClick = { navController.navigate(Screen.SplashScreen.route) }) {
+            Text(text = "To Home Screen")
+
+        }
+    }
 }
--- app/src/main/java/com/rf/bandmeets/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/rf/bandmeets/ui/screens/SplashScreen.kt
@@ -1,14 +1,56 @@
 package com.rf.bandmeets.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
-fun SplashScreen(navController: NavController){
-    Button(onClick = {navController.navigate(Screen.MainScreen.route)}){
-        Text(text="To Home Screen")
+fun SplashScreen(navController: NavController) {
+    Scaffold(topBar = {
+        TopAppBar(elevation = 5.dp) {
+            Row(horizontalArrangement = Arrangement.Start) {
+                Spacer(modifier = Modifier.width(25.dp))
+
+                Text(
+                    text = "Login",
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+        }
+    }) {
+        Button(onClick = { navController.navigate(Screen.MainScreen.route) }) {
+            Text(text = "To Home Screen")
+        }
+        /*    TopAppBar(elevation = 5.dp) {
+                Row(horizontalArrangement = Arrangement.Start){
+                    Icon(imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Arrow Back",
+                        modifier=Modifier.clickable{
+                    })
+                    Spacer(modifier = Modifier.width(25.dp))
+
+                    Text(
+                        text="Login",
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+                }
+            }
+
+         */
     }
+
+    /*
+
+     */
 
 }
