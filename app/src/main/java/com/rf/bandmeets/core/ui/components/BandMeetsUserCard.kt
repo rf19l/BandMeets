2c993ec77af9b9c53035bfe57191ee3f19a9a1a4
Ryan Foster <ryan.jfoster94@gmail.com>
2022-06-26 19:52:26 -0400

    Made updates to home screen and user card component

--- app/src/main/java/com/rf/bandmeets/core/ui/components/BandMeetsUserCard.kt
+++ app/src/main/java/com/rf/bandmeets/core/ui/components/BandMeetsUserCard.kt
@@ -1,62 +1,112 @@
 package com.rf.bandmeets.core.ui.components
 
-import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rf.bandmeets.R
-import com.rf.bandmeets.core.ui.theme.BandMeetsTheme
 
 @Composable
-fun BandMeetsUserCard(){
+fun BandMeetsUserCard(
+    modifier: Modifier = Modifier
+) {
     Box(
-        modifier = Modifier.fillMaxSize(.9f),
+        modifier = Modifier
+            .fillMaxSize(.9f)
+            .wrapContentSize(),
         contentAlignment = Alignment.Center,
-    ){
+    ) {
         Column(
             Modifier
                 .background(
-                    color=MaterialTheme.colorScheme.primaryContainer,
+                    color = MaterialTheme.colorScheme.primaryContainer,
                     shape = MaterialTheme.shapes.extraLarge,
                 )
                 .padding(8.dp)
-                .align(Alignment.TopCenter)
+                .align(Alignment.TopCenter),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
 
-        ){
+        ) {
             Image(
-                painterResource(id = R.drawable.avatar ),
+                painterResource(id = R.drawable.avatar),
                 contentDescription = "User Avatar",
-                modifier = Modifier.fillMaxSize(.7f)
-            )
-            Text("Text")
-            Row(
                 modifier = Modifier
-                    .padding(10.dp)
+                    .fillMaxWidth(.7f)
+                    .clip(CircleShape)
+                    .background(color = Color.Transparent, shape = CircleShape),
 
-            ) {
-                Text(
-                    text= "Name",
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
                 )
-                Text(text = "Info",
-                    color = MaterialTheme.colorScheme.onPrimaryContainer)
-                
-            }
+            Text("Name", color = MaterialTheme.colorScheme.onPrimaryContainer)
+            Text(
+                text = "Name", color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
         }
 
 
     }
-    
+
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardTest() {
+    Card(
+        elevation = CardDefaults.cardElevation(4.dp, 4.dp, 4.dp, 4.dp, 4.dp, 4.dp),
+        modifier = Modifier
+            .background(
+                color = MaterialTheme.colorScheme.primaryContainer,
+                shape = MaterialTheme.shapes.medium,
+            )
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(8.dp),
+        shape = MaterialTheme.shapes.medium,
+
+        ) {
+        Column(
+            Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painterResource(id = R.drawable.avatar),
+                contentDescription = "User Avatar",
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(
+                        color = MaterialTheme.colorScheme.primaryContainer,
+                        shape = MaterialTheme.shapes.large
+                    )
+                    .align(CenterHorizontally)
+                    .fillMaxWidth(.5f),
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "Name", color = MaterialTheme.colorScheme.onPrimaryContainer,
+                modifier = Modifier
+                    .align(CenterHorizontally)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.headlineSmall
+
+
+            )
+        }
+    }
+}
+
 
 @Preview(
     name = "User Card",
@@ -64,6 +114,19 @@ fun BandMeetsUserCard(){
 @Composable
 @Suppress("UnusedPrivateMember")
 private fun CardViewPreview() {
-            BandMeetsUserCard()
+    Surface {
+        BandMeetsUserCard()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(
+    name = "Standard Card"
+)
+@Composable
+fun Test() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        CardTest()
+    }
 
 }
--- app/src/main/java/com/rf/bandmeets/home/ui/HomeContent.kt
+++ app/src/main/java/com/rf/bandmeets/home/ui/HomeContent.kt
@@ -1,47 +1,179 @@
 package com.rf.bandmeets.home.ui
 
-import androidx.activity.OnBackPressedCallback
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.rf.bandmeets.core.ui.Screen
-import com.rf.bandmeets.home.ui.HomeViewState
-import com.rf.bandmeets.navigation.NavigationUtils
+import com.rf.bandmeets.R
+import com.rf.bandmeets.core.ui.components.CardTest
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeContent(
     viewState: HomeViewState,
     onBackPressed: () -> Unit,
 ) {
-    Scaffold(topBar = {
-        TopAppBar(elevation = 5.dp) {
-            Row(horizontalArrangement = Arrangement.Start) {
-                Icon(imageVector = Icons.Default.ArrowBack,
-                    contentDescription = "Arrow Back",
-                    modifier = Modifier.clickable {
-                       onBackPressed()
-                    })
-                Spacer(modifier = Modifier.width(25.dp))
+    Scaffold(
 
-                Text(
-                    text = "Home",
-                    modifier = Modifier.align(Alignment.CenterVertically)
+        topBar = {
+            MyToolBar()
+        },
+        content = {
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background),
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(it)
+                        .fillMaxWidth(.7f)
+                        .fillMaxHeight(.5f),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CardTest()
+                }
+            }
+        },
+        bottomBar = {
+            BottomNavigation(
+                backgroundColor = MaterialTheme.colorScheme.primary
+            ) {
+                BottomNavigationItem(
+                    icon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_android_black_24dp),
+                            contentDescription = "item.title"
+                        )
+                    },
+                    label = { Text(text = "item.title") },
+                    selectedContentColor = MaterialTheme.colorScheme.primary,
+                    unselectedContentColor = MaterialTheme.colorScheme.inversePrimary,
+                    alwaysShowLabel = true,
+                    selected = true,
+                    onClick = {}
+                )
+                BottomNavigationItem(
+                    icon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_android_black_24dp),
+                            contentDescription = "item.title"
+                        )
+                    },
+                    label = { Text(text = "item.title") },
+                    selectedContentColor = MaterialTheme.colorScheme.primary,
+                    unselectedContentColor = MaterialTheme.colorScheme.inversePrimary,
+                    alwaysShowLabel = true,
+                    selected = false,
+                    onClick = {}
+                )
+                BottomNavigationItem(
+                    icon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_android_black_24dp),
+                            contentDescription = "item.title"
+                        )
+                    },
+                    label = { Text(text = "item.title") },
+                    selectedContentColor = MaterialTheme.colorScheme.primary,
+                    unselectedContentColor = MaterialTheme.colorScheme.inversePrimary,
+                    alwaysShowLabel = true,
+                    selected = false,
+                    onClick = {}
+                )
+
+            }
+
+        },
+
+        )
+
+}
+
+
+@Composable
+private fun MyToolBar(
+) {
+    val toolbarHeight = 84.dp
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .statusBarsPadding()
+                .height(toolbarHeight)
+        ) {
+            IconButton(
+                onClick = { },
+            ) {
+                Icon(
+                    Icons.Default.KeyboardArrowLeft,
+                    contentDescription = ("test"),
+                    modifier = Modifier
+                        .size(toolbarHeight),
+                )
+            }
+
+            Crossfade(
+                targetState = "title",
+                modifier = Modifier
+                    .padding(vertical = 16.dp)
+                    .clickable(
+                        onClick = { },
+                    )
+                    .weight(1F)
+                    .height(toolbarHeight),
+            ) { title ->
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    Text(
+                        text = title,
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.headlineMedium,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.align(Alignment.Center),
+                    )
+                }
+            }
+
+            IconButton(
+                onClick = {},
+                modifier = Modifier
+                    .testTag("test tag"),
+            ) {
+                Icon(
+                    Icons.Default.KeyboardArrowRight,
+                    contentDescription = ("test"),
+                    modifier = Modifier
+                        .size(toolbarHeight),
                 )
             }
         }
-    }) {
-        Button(onClick = { onBackPressed() }) {
-            Text(text = "To Home Screen")
-
-        }
     }
+}
+
+@Preview(
+    name = "Home Preview"
+)
+@Composable
+fun HomePreview() {
+    HomeContent(viewState = HomeViewState.Initial, onBackPressed = {})
 }
